9c15a27dbfea62af1e9180a19177bfcbc7f922cf
rdinnager <r.dinnage@gmail.com>
2021-11-09 22:11:35 -0500

    added function to create `impac` function with access to various metadata

---
+++ R/func.R
@@ -0,0 +1,39 @@
+# Takes a function or an expression (wrapped in {}), and makes it into a function
+# which can take all the metadata available from impac.
+make_impac_func <- function(x) {
+
+  y <- rlang::enquo(x)
+  express <- rlang::quo_get_expr(y)
+
+  if(!inherits(express, "{")) {
+    x <- rlang::eval_tidy(y)
+  } else {
+    x <- express
+  }
+
+  fun_vars <- rlang::pairlist2(.x = ,
+                               .y = ,
+                               .i = ,
+                               .s = ,
+                               .meta = ,
+                               .img = ,
+                               .np = ,
+                               ... = )
+
+  if(rlang::is_expression(x)) {
+    body_ <- x
+  } else {
+    if(rlang::is_function(x)) {
+      body_ <- rlang::fn_body(x)
+      extra_args <- rlang::fn_fmls(x)
+      fun_vars <- c(extra_args, fun_vars)
+    } else {
+      stop("`x` must be a function or an R expression")
+    }
+  }
+
+  new_func <- rlang::new_function(fun_vars, body_)
+
+  new_func
+
+}
